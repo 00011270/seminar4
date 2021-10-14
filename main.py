ca87b40583ca843afa732d71bd710aad2606e8ef
00011270 <wiut11270@gmail.com>
2021-10-14 10:41:08 +0500

    Compressor Code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-print("What is your name")
-name = input()
-print("What is your surname")
-surname = input()
-print("Name " + name)
-print("Surname " + surname)
-jkj
+def encode(message):
+    encoded_message = ""
+    i = 0
+    while (i <= len(message) - 1):
+        count = 1
+        ch = message[i]
+        j = i
+        while (j < len(message) - 1):
+            if (message[j] == message[j + 1]):
+                count = count + 1
+                j = j + 1
+            else:
+                break
+        encoded_message = encoded_message + "*" + str(count) + ch
+        i = j + 1
+    return encoded_message
+
+
+encoded_message = encode("ABBBBCCCCCCCCAB")
+print(encoded_message) 
